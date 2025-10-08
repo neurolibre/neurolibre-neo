3f3bcb76860773c4c8762dd04bf8a0ebce224588
agahkarakuzu <agahkarakuzu@gmail.com>
2025-10-08 11:20:54 +0100

    Backfill existing records v1

--- db/migrate/20251008104715_add_version_to_papers.rb
+++ db/migrate/20251008104715_add_version_to_papers.rb
@@ -1,5 +1,12 @@
 class AddVersionToPapers < ActiveRecord::Migration[7.0]
   def change
-    add_column :papers, :version, :string
+    add_column :papers, :version, :string, default: 'v1'
+
+    # Backfill existing papers with v1
+    reversible do |dir|
+      dir.up do
+        Paper.where(version: nil).update_all(version: 'v1')
+      end
+    end
   end
 end
